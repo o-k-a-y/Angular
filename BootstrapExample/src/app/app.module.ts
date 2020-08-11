4a732935fbb954d5f3abeec2277667c78ca3e9e9
o-k-a-y <btunney@ramapo.edu>
2020-08-11 18:56:05 -0400

    Example new component use

--- BootstrapExample/src/app/app.module.ts
+++ BootstrapExample/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // Needed for ngModel
+
 import { AppComponent } from './app.component';
+// The new Component we created
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent // The new pOmponent we created. Now Angular knows about it so we can use it in our app.
   ],
   imports: [
-    FormsModule, // Needed for ngModel, now Angular knows we want to use some FormsModule features
-    BrowserModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
