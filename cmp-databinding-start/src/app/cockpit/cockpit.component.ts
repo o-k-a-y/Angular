5e4a8b655a08d29d9ad1eea21d9c39bec2481e68
o-k-a-y <btunney@ramapo.edu>
2020-09-04 12:56:48 -0400

    Added alias to component variable

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-
   newServerName = '';
   newServerContent = '';
 
--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -7,11 +7,12 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
   // @Input exposes the property (now it's publicly accessible from other components)
-  @Input() element: {type: string, name: string, content: string};
+  // In paranthesis we make an alias of what this variable must be referenced by in any component other than itself
+  //    in order to bind to it
+  @Input('srvElement') element: {type: string, name: string, content: string};
 
-  test: string;
-
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit(): void {
   }
