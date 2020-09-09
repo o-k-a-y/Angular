41ed1d942d9dada9826e146719a9f31a14eb7161
o-k-a-y <btunney@ramapo.edu>
2020-09-08 20:37:15 -0400

    ViewEncapsulation to show how CSS can be applied globally on the DOM regardless of Component

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+  
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None // Makes the css applied to this component affect the global DOM (probably not what you want almost always)
 })
 export class ServerElementComponent implements OnInit {
   // @Input exposes the property (now it's publicly accessible from other components)
