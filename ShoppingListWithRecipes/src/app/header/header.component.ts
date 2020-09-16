b19ec05f747d3fa209b3e3da31c294b3a93f3070
o-k-a-y <btunney@ramapo.edu>
2020-09-15 22:20:56 -0400

    recipe-list component now creates recipe-items rather than creating the html itself

--- ShoppingListWithRecipes/src/app/header/header.component.ts
+++ ShoppingListWithRecipes/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
+
+
   collapsed = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // When the shopping list or recipes navbar buttons are clicked,
+  // featureSelected event will fire
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
   }
 
 }
