0c74ec1b0f89486710fdfa7e672355c8914c477c
o-k-a-y <btunney@ramapo.edu>
2020-09-19 22:28:59 -0400

    Better custom directive using Renderer2 and implementing channges in ngOnInit()

--- directives-start/src/app/basic-highlight/basic-highlight.directive.ts
+++ directives-start/src/app/basic-highlight/basic-highlight.directive.ts
@@ -8,7 +8,9 @@ export class BasicHighlightDirective implements OnInit {
     constructor(private elementRef: ElementRef) {}
 
     ngOnInit() {
+        // Better to use renderer instead of direct access because if running angular outside the browser more errors can occur
         this.elementRef.nativeElement.style.backgroundColor = "green";
+        this.elementRef.nativeElement.style.color = "white";
     }
 
 }
---
+++ directives-start/src/app/better-hightlight/better-highlight.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit() {
+    // Better to use renderer because if running angular outside the browser more errors can occur
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white');
+  }
+
+}
