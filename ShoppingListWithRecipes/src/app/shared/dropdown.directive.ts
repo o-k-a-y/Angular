1548fb2b99c0e10c1287210a25456a6125c381fb
o-k-a-y <btunney@ramapo.edu>
2020-09-24 22:17:32 -0400

    Another alternative to closing dropdown by closing tlobally when clicked off a dropdown

--- ShoppingListWithRecipes/src/app/shared/dropdown.directive.ts
+++ ShoppingListWithRecipes/src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, HostBinding, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
     selector: '[appDropdown]'
@@ -10,8 +10,22 @@ export class DropdownDirective {
     @HostBinding('class.open')
     isOpen = false;
 
+    // When document is clicked off a dropdown, close the dropdown
     // Listen to click event on the element this directive is attached to
-    @HostListener('click') toggleOpen() {
-        this.isOpen = !this.isOpen;
+    @HostListener('document:click', ['event']) toggleOpen() {
+        console.log(event.target);
+        console.log(this.elRef.nativeElement);
+        let test = this.elRef.nativeElement.toString().split("target")
+        console.log(test);
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
     }
+
+    // // Listen to click event on the element this directive is attached to
+    // @HostListener('click') toggleOpen() {
+    //     this.isOpen = !this.isOpen;
+    // }
+
+
+
+    constructor(private elRef: ElementRef) {}
 }
