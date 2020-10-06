ef1d530cca9fb06f25352997846e6d186ca284de
o-k-a-y <btunney@ramapo.edu>
2020-10-05 21:02:18 -0400

    services-start: Updated to show how we can inject services at the module level, and inject services into other services

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -1,24 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { stat } from 'fs';
 
+import { AccountsService } from '../accounts.service';
 import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService, 
+              private accountsService: AccountsService) {}
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-
-    this.loggingService.logStatusChanged(status);
+    this.accountsService.updateStatus(this.id, status);
+    // this.loggingService.logStatusChanged(status);
 
     // console.log('A server status changed, new status: ' + status);
   }
--- services-start/src/app/app.component.ts
+++ services-start/src/app/app.component.ts
@@ -1,31 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // providers: [AccountsService]
 })
-export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
+export class AppComponent implements OnInit {
+  // Array of json
+  accounts: {name: string, status: string}[] = [];
+  
+  constructor(private accountsService: AccountsService) {}
 
-  onAccountAdded(newAccount: {name: string, status: string}) {
-    this.accounts.push(newAccount);
-  }
 
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  ngOnInit() {
+    // Remember, arrays in JS are references so we are referencing the same array in AccountsService
+    this.accounts = this.accountsService.accounts;
   }
+  
 }
