d8f5877cb03e211edf3c318ad728a9731be56c07
o-k-a-y <22223097+o-k-a-y@users.noreply.github.com>
2020-10-29 23:06:23 -0400

    navigate method example showing how we would access a relative path to a route

--- routing-start/src/app/servers/servers.component.ts
+++ routing-start/src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ServersService } from './servers.service';
 
 @Component({
@@ -9,10 +10,19 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    // The navigate method doesn't know what route we're currently on 
+    // The routerLink always knows which component (template) it lives in and therefore it knows what the currently loaded route is
+    
+    // this.router.navigate(["servers"], {relativeTo: this.route});
   }
 
 }
